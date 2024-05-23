5b243951428d738a0ca4d53a6ca65cb42e690d80
leomiclos <leonardor869@gmail.com>
2024-05-23 11:37:55 -0300

    V3

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,16 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PoMenuItem, PoMenuModule } from '@po-ui/ng-components';
+import { PoMenuItem, PoMenuModule, PoPageModule, PoToolbarModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, PoMenuModule],
+  imports: [
+    RouterOutlet,
+    PoMenuModule,
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule,
+    HttpClientModule,
+  ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Home'},
-  ];
+  readonly menus: Array<PoMenuItem> = [{ label: 'Home' }];
 }
